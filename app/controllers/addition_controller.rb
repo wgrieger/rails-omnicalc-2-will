36c677345ec5308580053dbca5fdd5a7add4d7d1
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-04-25 02:09:48 +0000

    making progress

--- app/controllers/addition_controller.rb
+++ app/controllers/addition_controller.rb
@@ -1,10 +1,10 @@
 class AdditionController < ApplicationController
   def show_addition_form
-    render({ :template => "addition_templates/add_form" })
+    render({ :template => "addition_templates/addition_form" })
   end
 
   def add_these
-    @first_number = params.fetch("first_number").to_f
+    @first_number = params.fetch("first_num").to_f
     @second_number = params.fetch("second_num").to_f
 
     @result = @first_number + @second_number
